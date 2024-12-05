9741873249ab78e779d86047e5fa6463381912f1
andrey_kir <andrey.kirillov.kiev@gmail.com>
2024-12-05 22:04:25 +0200

    test cloudbuild.yaml

--- index.unit.test.js
+++ index.unit.test.js
@@ -2,6 +2,6 @@ const getSum = require('./index.js');
 
 describe('getSum', () => {
     it('should return the sum of two numbers', () => {
-        expect(getSum(1, 2)).toBe(3);
+        expect(getSum(1, 2)).toBe(4);
     });
 });
